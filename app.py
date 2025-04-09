db33db6763cae180f058a8aeb61f5f5aee0dd22f
amir-esfahani <ad.esfahanizadeh@yahoo.com>
2025-04-09 23:57:46 +0330

    initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from extensions import db
+from models import User
+from auth import auth_bp
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+app.register_blueprint(auth_bp)
+
+
+with app.app_context():
+    users = db.session.query(User).all()
+    for user in users:
+        print(f"User name: {user.username}, pass: {user.password_hash}")
+
+# with app.app_context():
+#     db.create_all()  # Creates the user table if it doesn't exist
+
+#     # Create a user record
+#     user = User(username="manager")
+#     user.set_password("manager123")
+#     db.session.add(user)
+#     db.session.commit()
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ auth.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, request, jsonify
+from models import User
+from extensions import db
+import jwt
+import datetime
+import os
+
+auth_bp = Blueprint('auth', __name__)
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-dev-key')
+
+@auth_bp.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    user = User.query.filter_by(username=username).first()
+    print("User:", user.username if user else "None")
+    print("Password:", password)
+    print(user.check_password(password) if user else "None")
+    if user and user.check_password(password):
+        token = jwt.encode({
+            'user_id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+        }, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'token': token})
+
+    return jsonify({'error': 'Invalid credentials'}), 401
